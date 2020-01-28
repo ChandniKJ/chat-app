018b21a96a24e9f7db212caf6d805bd40b4449d6
Chandni Janachandran <chandni.janachandran@gmail.com>
2020-01-28 14:37:55 +0000

    Add Navbar and Quotecard

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="Navbar">
+      <h1>Simpon's Quotes</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
