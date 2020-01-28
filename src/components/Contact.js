4cd7bab5cac9715a2f9cd9bea695ee1a39c308b1
Chandni Janachandran <chandni.janachandran@gmail.com>
2020-01-28 17:12:38 +0000

    Modify Contact file

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import './Contact.css'
-
+import './Contact.css';
 function Contact(props) {
-  return (
+    return  (
     <div className="Contact">
-      <img src={props.avatar} className="avatar" />
-      <h4 className="name">{props.name}</h4>
-      <div className="status">
-        {props.online ? 'Online' : 'Offline'} />
-        <span className ="status-text">
-          {props.online ? 'Online' : 'Offline'}
-        </span>
-      </div> 
-    </div>
-       );
+          <img src={props.avatar} className="avatar"/>
+          <div>
+            <h4 className="name">{props.name}</h4>
+            <div className="status">
+                <div className={props.online ? "status-online" : "status-offline"} />
+                <span className="status-text">
+                    {props.online ? 'Online' : 'Offline'}
+                </span>
+            </div>
+          </div>
+      </div>
+      );      
 }
-
 export default Contact;
-
-  
