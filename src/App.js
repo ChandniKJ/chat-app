4769844b060e4af322e36daedc8a181c00bc79e9
Chandni Janachandran <chandni.janachandran@gmail.com>
2020-01-30 15:40:21 +0000

    Rename file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ContactList from './components/ContactList';
+import ContactList from './components/contactList';
 import Contact from './components/Contact'
 
 function App() {
