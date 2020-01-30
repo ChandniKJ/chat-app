4cdc050d8e50a4a7fc97c899df6f4bed877e869f
Chandni Janachandran <chandni.janachandran@gmail.com>
2020-01-30 15:20:50 +0000

    Update contact list

--- src/components/contactList.js
+++ src/components/contactList.js
@@ -1,45 +1,47 @@
 import React from 'react';
 import Contact from './Contact'
 
+function ContactList(props){
+    
+  return (
+    <div>
+        {user.map((user) => (
+            <Contact
+                 name={user.name}
+                 avatar={user.avatar}
+                 online={user.online}
+            />
+        ))}
+    </div>
+ );
+ }
+
 const user = [
   {
-    id: 1,
     avatar:"https://randomuser.me/api/portraits/women/39.jpg",
     name: "Colleen Crawford",
-    online: true
+    online: true,
   },
   {
-    id: 2,
     avatar: "https://randomuser.me/api/portraits/men/83.jpg",
     name: "Donald Rhodes",
-    online: false
+    online: false,
   },
   {
-    id: 3,
     avatar: "https://randomuser.me/api/portraits/women/25.jpg",
     name: "Wanda Douglas",
-    online: true
+    online: true,
   },
   {
-    id: 4,
     avatar:"https://randomuser.me/api/portraits/men/35.jpg",
     name: "Ruben May",
-    online: false
+    online: false,
   },
   {
-    id: 5,
     avatar:"https://randomuser.me/api/portraits/women/57.jpg",
     name: "Claudia Fuller",
-    online: true
+    online: true,
   },
 ];
   
-  const contactList = () => (
-    <div>
-      {user.map(item => (
-      <Contact key={item.id} name={item.name} avatar={item.avatar} online={item.online} />
-      ))}
-    </div>
-  );
-  
-  export default contactList;
+  export default ContactList;
