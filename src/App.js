342bbe754c935cad02ca5afe97c4d2e4a8af26cd
ChandniKJ <54037953+ChandniKJ@users.noreply.github.com>
2020-02-05 09:37:34 +0000

    Show user status (online/offline)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ContactList from './components/contactList';
-
+import Contact from './components/Contact'
 
 function App() {
   return (
+    <div className="App">
       <ContactList/>
+      <Contact
+        avatar="https://randomuser.me/api/portraits/men/64.jpg"
+        name="Steve Weaver"
+        online={false}
+      />
+      <Contact
+        avatar="https://randomuser.me/api/portraits/women/64.jpg"
+        name="Judy Walters"
+        online={true}
+      />
+      <Contact
+         avatar="https://randomuser.me/api/portraits/men/3.jpg"
+         name="Dale Mills"
+         online={false}
+      />
+    </div>
   );
 }
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import './Contact.css';
 
-function Contact(props) {
+class Contact extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        online: props.online,
+      };
+    }
+
+render() {
     return  (
     <div className="Contact">
-          <img src={props.avatar} className="avatar"/>
+          <img src={this.props.avatar} alt={this.props.name}className="avatar"/>
           <div>
-            <h4 className="name">{props.name}</h4>
+            <h4 className="name">{this.props.name}</h4>
             <div className="status">
-                <div className={props.online ? "status-online" : "status-offline"} />
+                <div className={this.state.online ? "status-online" : "status-offline"}
+                onClick={event => {
+                    const userStatus = !this.state.online;
+                    this.setState({ online: userStatus });
+                  }}
+                />
                 <span className="status-text">
-                    {props.online ? 'Online' : 'Offline'}
+                    {this.state.online ? 'Online' : 'Offline'}
                 </span>
             </div>
           </div>
       </div>
-      );      
+      );
+}
 }
 export default Contact;
